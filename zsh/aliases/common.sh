34cd8f5d3a9947a94e1432d02d8fcf258397e034
Jonatha Gabriel <jonathagabrielns@gmail.com>
2021-12-23 15:57:26 -0300

    Zsh configuration and VIM adjusts
    Changes:
        - Changed to install script instead of a makefile
        - Some minor adjusts on VIM configuration
    
    Added:
        - Zsh aliases for common commands and basic git aliases
        - Added Zsh options
        - Enabled Zsh history file
        - Updated some environment variables

---
+++ zsh/aliases/common.sh
@@ -0,0 +1,29 @@
+ #################
+##
+#     Aliases     #
+                 ##
+ #################
+
+## Ls
+alias l="ls -CF"
+
+alias la="ls -AF"
+alias lr="ls -RF"
+alias ll="ls -hlF"
+
+alias lar="ls -ARF"
+alias lla="ls -hlAF"
+alias llr="ls -hlRF"
+
+alias llar="ls -hlARF"
+
+## Mkdir
+alias md="mkdir -p"
+
+## Rm
+alias rm="rm -f"
+alias rd="rm -rf"
+
+## Clear
+alias cl="clear"
+
---
+++ zsh/aliases/git.sh
@@ -0,0 +1,21 @@
+ #################
+##
+#   Git Aliases   #
+                 ##
+ #################
+
+## Git
+alias g="git"
+
+## Git add
+alias ga="git add"
+
+## Git diff
+alias gd="git diff"
+
+## Git status
+alias gs="git status"
+
+## Git commit
+alias gc="git commit"
+
