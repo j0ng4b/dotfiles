f5e367b451c2357508a8c1bb297a0dce0d727083
Jonatha Gabriel <jonathagabrielns@gmail.com>
2023-11-09 21:09:56 -0300

    Added spawn in current directory.

---
+++ zsh/config/utils/chpwd.zsh
@@ -0,0 +1,16 @@
+# This will be used to spawn a terminal in current directory
+function osc7-pwd() {
+    emulate -L zsh
+    setopt extendedglob
+
+    local LC_ALL=C
+
+    printf '\e]7;file://%s%s\e\' $HOST ${PWD//(#m)([^@-Za-z&-;_~])/%${(l:2::0:)$(([##16]#MATCH))}}
+}
+
+function chpwd-osc7-pwd() {
+    (( ZSH_SUBSHELL )) || osc7-pwd
+}
+
+add-zsh-hook -Uz chpwd chpwd-osc7-pwd
+
