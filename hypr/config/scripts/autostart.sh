f57b4ba1d0d11149cee26e6012f1961553b3ce0b
Jonatha Gabriel <jonathagabrielns@gmail.com>
2024-04-27 20:07:43 -0300

    update autostart script

--- hypr/config/scripts/autostart.sh
+++ hypr/config/scripts/autostart.sh
@@ -3,21 +3,40 @@
 root_dir=$(cd $(dirname $0); pwd)
 
 ## Start pipewire
-pgrep -f pipewire || pipewire &
+if [ $(pgrep -f pipewire) ]; then
+    # With this PipeWire will automatic start pipewire-pulse and wireplumber
+    # if the PipeWire configuration is installed
+    pipewire &
+fi
 
 ## Start D-Bus
-dbus="dbus-daemon --session --address=unix:path=$XDG_RUNTIME_DIR/bus"
-pgrep -fx "$dbus" || $dbus &
+#dbus="dbus-daemon --session --address=unix:path=$XDG_RUNTIME_DIR/bus"
+#if [ $(pgrep -fx "$dbus") ]; then
+#    $dbus &
+#fi
 
 ## Start mako notification server
-pgrep -f mako || mako &
+#if [ $(pgrep -f mako) ]; then
+#    mako &
+#fi
 
 ## Start battery notifier
-pgrep -f battery_notifier.sh || $root_dir/battery_notifier.sh &
+if [ $(pgrep -f battery_notifier.sh) ]; then
+    $root_dir/battery_notifier.sh &
+fi
 
 ## Start swww daemon
-pgrep -f swww-daemon || swww init
+if [ $(pgrep -f swww-daemon) ]; then
+    swww-daemon &
+fi
 
 ## Start eww
-pgrep -f eww || eww daemon
+if [ $(pgrep -f eww) ]; then
+    eww daemon
+fi
+
+eww open bar
+eww open corner-left
+eww open corner-right
+eww reload # needed to adjusts the window corners
 
