8e31b7803eb66e7a05b5ef357b4902ab539bb59e
Jonatha Gabriel <jonathagabrielns@gmail.com>
2024-04-28 00:48:47 -0300

    update autostart.sh and migrate to dunst

--- config/hypr/scripts/autostart.sh
+++ config/hypr/scripts/autostart.sh
@@ -3,35 +3,30 @@
 root_dir=$(cd $(dirname $0); pwd)
 
 ## Start pipewire
-if [ $(pgrep -f pipewire) ]; then
+if [ -z $(pgrep -f pipewire) ]; then
     # With this PipeWire will automatic start pipewire-pulse and wireplumber
     # if the PipeWire configuration is installed
     pipewire &
 fi
 
-## Start D-Bus
-#dbus="dbus-daemon --session --address=unix:path=$XDG_RUNTIME_DIR/bus"
-#if [ $(pgrep -fx "$dbus") ]; then
-#    $dbus &
-#fi
-
-## Start mako notification server
-#if [ $(pgrep -f mako) ]; then
-#    mako &
-#fi
+## Start dunst notification server
+if [ -z $(pgrep -f dunst) ]; then
+    dunst &
+fi
 
 ## Start battery notifier
-if [ $(pgrep -f battery_notifier.sh) ]; then
+if [ -z $(pgrep -f battery_notifier.sh) ]; then
+    echo $root_dir
     $root_dir/battery_notifier.sh &
 fi
 
 ## Start swww daemon
-if [ $(pgrep -f swww-daemon) ]; then
+if [ -z $(pgrep -f swww-daemon) ]; then
     swww-daemon &
 fi
 
 ## Start eww
-if [ $(pgrep -f eww) ]; then
+if [ -z $(pgrep -f eww) ]; then
     eww daemon
 fi
 
