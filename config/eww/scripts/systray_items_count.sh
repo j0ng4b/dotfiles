4d9ab5d3bcd3af53335e4c3e55751a812f327561
Jonatha Gabriel <jonathagabrielns@gmail.com>
2024-05-24 01:57:59 -0300

    add eww systray

---
+++ config/eww/scripts/systray_items_count.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+
+# Scripts directory root
+root=$(cd $(dirname $0); pwd)
+
+## Cache
+systray_cache_dir=${XDG_CACHE_HOME:-${HOME}/.cache}/eww/systray
+systray_count=$systray_cache_dir/count
+
+if [ ! -d "$systray_cache_dir" ]; then
+    mkdir -p "$systray_cache_dir"
+    echo "0" > $systray_count
+fi
+
+# On system reboot reset the counter
+system_uptime=$(date -d "$(uptime -s)" +%s)
+current_time=$(date +%s)
+if [ $(( ($current_time - $system_uptime) / 60 )) -le 5 ]; then
+    echo "0" > $systray_count
+fi
+
+count=$(cat $systray_count)
+
+dbus-monitor --session "interface='org.kde.StatusNotifierWatcher'" |
+while read -r signal; do
+    case "$signal" in
+        *"StatusNotifierItemRegistered"*)
+            count=$(($count + 1))
+            ;;
+
+        *"StatusNotifierItemUnregistered"*)
+            count=$(($count - 1))
+            ;;
+    esac
+
+    [ $count -lt 0 ] && count=0
+    echo $count | tee $systray_count
+done
