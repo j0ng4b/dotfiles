14020af744a183302cc611079680a9e8d8fd26c4
Jonatha Gabriel <jonathagabrielns@gmail.com>
2024-05-29 02:48:44 -0300

    update battery script and others indicators

--- config/eww/scripts/indicator/battery.sh
+++ config/eww/scripts/indicator/battery.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env sh
 
-capacity_file="/sys/class/power_supply/BAT0/capacity"
-status_file="/sys/class/power_supply/BAT0/status"
+batteries=$(find /sys/class/power_supply/ -name "BAT*")
+if [ $(echo $batteries | wc -l) -gt 0 ]; then
+    enable=1
+
+    for bat in $batteries; do
+        capacity_file="$bat/capacity"
+        status_file="$bat/status"
+
+        break
+    done
+else
+    enable=0
+fi
 
 
 status=$(cat "$status_file" | sed -e 's/.*/\L&/')
 capacity=$(cat "$capacity_file")
 
-echo "{ \"capacity\": \"$capacity\", \"status\": \"$status\" }"
+echo "{ \"enable\": \"$enable\", \"capacity\": \"$capacity\", \"status\": \"$status\" }"
