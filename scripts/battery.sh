bfcc39a63efd5e64477d947ae9f9b7687e1fdc35
Jonatha Gabriel <jonathagabrielns@gmail.com>
2024-07-05 23:19:43 -0300

    added battery script

---
+++ scripts/battery.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env sh
+
+batteries=$(find /sys/class/power_supply/ -name "BAT*")
+if [ $(echo $batteries | wc -l) -gt 0 ]; then
+    enable=1
+
+    for bat in $batteries; do
+        capacity_file="$bat/capacity"
+        status_file="$bat/status"
+
+        break
+    done
+else
+    enable=0
+fi
+
+
+if [ $enable -eq 1 ]; then
+    status=$(cat "$status_file" | sed -e 's/.*/\L&/')
+    capacity=$(cat "$capacity_file")
+else
+    error 'No battery!'
+    exit 1
+fi
+
+_battery_capacity_icon() {
+    case "$(($capacity / 10))" in
+        10) echo '󰁹' ;;
+         9) echo '󰂂' ;;
+         8) echo '󰂁' ;;
+         7) echo '󰂀' ;;
+         6) echo '󰁿' ;;
+         5) echo '󰁾' ;;
+         4) echo '󰁽' ;;
+         3) echo '󰁼' ;;
+         2) echo '󰁻' ;;
+         1) echo '󰁺' ;;
+         *) echo '󰂎' ;;
+    esac
+}
+
+_battery_charging_icon() {
+    case "$(($capacity / 10))" in
+        10) echo '󰂅' ;;
+         9) echo '󰂋' ;;
+         8) echo '󰂊' ;;
+         7) echo '󰢞' ;;
+         6) echo '󰂉' ;;
+         5) echo '󰢝' ;;
+         4) echo '󰂈' ;;
+         3) echo '󰂇' ;;
+         2) echo '󰂆' ;;
+         1) echo '󰢜' ;;
+         *) echo '󰢟' ;;
+    esac
+}
+
+case $1 in
+    status)
+        echo "{ \"enable\": \"$enable\", \"capacity\": \"$capacity\", \"status\": \"$status\" }"
+        ;;
+
+    capacity)
+        echo $capacity
+        ;;
+
+    nerd)
+        if [ "$status" = "charging"  ]; then
+            echo $(_battery_charging_icon)
+        elif [ "$status" = "discharging" ]; then
+            echo $(_battery_capacity_icon)
+        elif [ "$status" = "not charging" ]; then
+            echo '󱈑'
+        elif [ "$status" = "full" ]; then
+            echo '󰂏'
+        fi
+        ;;
+esac
+
