a99e661ebacecb481a312fcf03feb077a917f13d
Jonatha Gabriel <jonathagabrielns@gmail.com>
2024-07-19 21:15:08 -0300

    better colorscheme config structure

---
+++ config/nvim/lua/colorscheme/init.lua
@@ -0,0 +1,17 @@
+-- TODO: colorscheme auto reload mechanics
+local colorscheme = 'catppuccin'
+
+-- Load colorscheme configurations
+local configs = vim.api.nvim_get_runtime_file('lua/colorscheme/config/*.lua', true)
+for _, config in ipairs(configs) do
+    if vim.fn.filereadable(config) == 1 then
+        require(config:match('.+/([^/]+%.lua)'):gsub('.lua', ''))
+    end
+end
+
+-- Load colorscheme
+local status, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
+if not status then
+    return
+end
+
