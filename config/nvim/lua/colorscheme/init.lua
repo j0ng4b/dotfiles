1a164c31b220cfcf7a2e04faa4e50e0458c41411
Jonatha Gabriel <jonathagabrielns@gmail.com>
2024-08-09 12:34:20 -0300

    feat[neovim]: color reloader, closes #39

--- config/nvim/lua/colorscheme/init.lua
+++ config/nvim/lua/colorscheme/init.lua
@@ -1,5 +1,5 @@
--- TODO: colorscheme auto reload mechanics
-local colorscheme = 'catppuccin'
+local auto = require('utils.autocmd')
+local json = require('utils.json')
 
 -- Load colorscheme configurations
 local configs = vim.api.nvim_get_runtime_file('lua/colorscheme/configs/*.lua', true)
@@ -11,16 +11,38 @@ for _, config in ipairs(configs) do
 end
 
 -- Load colorscheme
-local remap = {
-    gruvbox = 'gruvbox-material',
-}
+-- Default colorscheme
+local colorscheme = 'gruvbox-material'
 
-if remap[colorscheme] then
-    colorscheme = remap[colorscheme]
+local config_home = os.getenv('XDG_CONFIG_HOME') or '~/.config/'
+local config_file = vim.fs.joinpath(config_home, 'system-configs.json')
+local config = json.read(config_file)
+if config and config.theme then
+    colorscheme = config.theme
 end
 
 local status, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
 if not status then
     return
 end
+
+-- Trigger reloader on colorscheme change
+auto.group('ColorschemeReloader')
+auto.cmd('Colorscheme', '', function()
+    vim.schedule(function()
+        local config_home = os.getenv('XDG_CONFIG_HOME') or '~/.config/'
+        local config_file = vim.fs.joinpath(config_home, 'system-configs.json')
+        local config = json.read(config_file)
+
+        if config.theme ~= vim.g.colors_name then
+            if vim.g.colors_name == 'catppuccin-mocha' then
+                config.theme = 'catppuccin'
+            else
+                config.theme = vim.g.colors_name
+            end
+
+            json.write(config_file, config)
+        end
+    end)
+end, 'ColorschemeReloader')
 
