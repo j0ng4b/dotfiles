720f507c19f3aae9b309baa26b723f804363e8d1
Jonatha Gabriel <jonathagabrielns@gmail.com>
2025-01-12 22:20:51 -0300

    added: workspaces indicator

---
+++ config/eww/scripts/workspaces.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+__get_all_workspaces() {
+    workspaces_windows=$(swaymsg -t get_workspaces |
+        jq 'map({key: .name | tostring, value: [ .focused, true ]}) | from_entries')
+
+    seq 1 10 |
+        jq --argjson w "$workspaces_windows" \
+           --slurp \
+           --compact-output \
+           'map(tostring) | map({name: ., focused: ($w[.][0]//false), non_empty: ($w[.][1]//false)})'
+}
+
+echo "$(__get_all_workspaces)"
+swaymsg -t subscribe -m '[ "workspace" ]' | while read -r event; do
+    if [ "$(echo $event | jq --raw-output '.change')" = "focus" ]; then
+        echo "$(__get_all_workspaces)"
+    fi
+done
+
