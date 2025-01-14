7f47935321185e2fae35c216126a5141e788f022
Jonatha Gabriel <jonathagabrielns@gmail.com>
2025-01-13 22:48:07 -0300

    added: neocord, closes #44

---
+++ config/nvim/lua/plugins/neocord.lua
@@ -0,0 +1,31 @@
+local config = function()
+    require('neocord').setup({
+        -- General options
+        logo                = 'auto',
+        logo_tooltip        = nil,
+        main_image          = 'language',
+        client_id           = '1157438221865717891',
+        log_level           = nil,
+        debounce_timeout    = 10,
+        show_time           = true,
+        global_timer        = true,
+
+        -- Rich Presence text options
+        editing_text        = "Editing %s",
+        file_explorer_text  = "Browsing %s",
+        git_commit_text     = "Committing changes",
+        plugin_manager_text = "Managing plugins",
+        reading_text        = "Reading %s",
+        workspace_text      = "Working on %s",
+        line_number_text    = "Line %s out of %s",
+        terminal_text       = "Using Terminal",
+    })
+end
+
+
+return {
+    'IogaMaster/neocord',
+    event = 'VeryLazy',
+    config = config
+}
+
