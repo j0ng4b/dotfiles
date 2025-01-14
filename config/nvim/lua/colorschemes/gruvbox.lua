e6e722002ff7fe08ede548d972474550e758a7fa
Jonatha Gabriel <jonathagabrielns@gmail.com>
2025-01-13 23:38:37 -0300

    added: gruvbox

--- config/nvim/lua/colorschemes/gruvbox.lua
+++ config/nvim/lua/colorschemes/gruvbox.lua
@@ -1,24 +1,39 @@
 return {
-    'sainnhe/gruvbox-material',
-    name = 'gruvbox',
-    init = function()
-        vim.g.gruvbox_material_background = 'hard'
-        vim.g.gruvbox_material_foreground = 'original'
-        vim.g.gruvbox_material_statusline_style = 'original'
-
-        vim.g.gruvbox_material_show_eob = 0
-        vim.g.gruvbox_material_enable_bold = 1
-        vim.g.gruvbox_material_enable_italic = 1
-        vim.g.gruvbox_material_dim_inactive_windows = 1
-
-        vim.g.gruvbox_material_menu_selection_background = 'yellow'
-        vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
-        vim.g.gruvbox_material_inlay_hints_background = 'dimmed'
-        vim.g.gruvbox_material_spell_foreground = 'colored'
-        vim.g.gruvbox_material_ui_contrast = 'high'
-
-        vim.g.gruvbox_material_better_performance = 1
-    end,
+    'ellisonleao/gruvbox.nvim',
     priority = 1000,
+    config = true,
+    opts = {
+        terminal_colors = true,
+
+        strikethrough = true,
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+            strings = true,
+            emphasis = true,
+            comments = true,
+            operators = false,
+            folds = true,
+        },
+
+        inverse = true,
+        invert_signs = true,
+        invert_tabline = false,
+        invert_selection = false,
+        invert_intend_guides = false,
+
+        contrast = 'hard',
+        overrides = {
+            FoldColumn = { link = 'LineNr' },
+
+            CursorLineFold = { link = 'LineNr' },
+            CursorLineNr = { link = 'LineNr' },
+            CursorLineSign = { link = 'LineNr' },
+        },
+
+        dim_inactive = false,
+        transparent_mode = false,
+    }
 }
 
