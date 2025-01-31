01c2d112cb7b26a15669ec566dd2db97b71028b7
Jonatha Gabriel <jonathagabrielns@gmail.com>
2025-01-31 08:27:05 -0300

    fix(nvim): minimap closes on neo-tree

--- config/nvim/lua/plugins/minimap.lua
+++ config/nvim/lua/plugins/minimap.lua
@@ -15,6 +15,8 @@ return {
         vim.g.minimap_auto_start = 1
         vim.g.minimap_auto_start_win_enter = 1
         vim.g.minimap_background_processing = 1
+
+        vim.g.minimap_close_filetypes = { 'alpha', 'neo-tree' }
     end
 }
 
