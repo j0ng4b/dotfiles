c05cda9f1624ed59db77f058c880da29ecd57f3a
Jonatha Gabriel <jonathagabrielns@gmail.com>
2025-01-30 23:12:57 -0300

    feat(nvim): added minimap.vim and update plugins

---
+++ config/nvim/lua/plugins/minimap-vim.lua
@@ -0,0 +1,20 @@
+return {
+    'wfxr/minimap.vim',
+    init = function()
+        vim.g.minimap_width = 20
+
+        -- Git highlight
+        vim.g.minimap_git_colors = 1
+
+        -- Range highlight
+        vim.g.minimap_highlight_range = 1
+
+        -- Search highlight
+        vim.g.minimap_highlight_search = 1
+
+        vim.g.minimap_auto_start = 1
+        vim.g.minimap_auto_start_win_enter = 1
+        vim.g.minimap_background_processing = 1
+    end
+}
+
