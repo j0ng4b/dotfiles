4d7dfe2ea2d806da0d8ff69b5e0ed228dfeb7099
Jonatha Gabriel <jonathagabrielns@gmail.com>
2025-02-01 09:22:00 -0300

    feat(nvim): added animations to nvim :)

---
+++ config/nvim/lua/plugins/mini-animate.lua
@@ -0,0 +1,9 @@
+return {
+    'echasnovski/mini.animate',
+    opts = {
+        cursor = {
+            enable = false,
+        },
+    },
+}
+
---
+++ config/nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,17 @@
+return {
+    'sphamba/smear-cursor.nvim',
+    opts = {
+        time_interval = 15,
+        hide_target_hack = false,
+        legacy_computing_symbols_support = true,
+
+        stiffness = 0.7,
+        trailing_stiffness = 0.4,
+        trailing_exponent = 5,
+        distance_stop_animating = 0.35,
+
+        cursor_color = '#df6000',
+        gamma = 3,
+    },
+}
+
