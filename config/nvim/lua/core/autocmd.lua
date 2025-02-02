c1fd597c6ae3754858440cdf0bb7256f39197afe
Jonatha Gabriel <jonathagabrielns@gmail.com>
2025-02-01 21:59:11 -0300

    fix(nvim): minimap not showing search highlights

--- config/nvim/lua/core/autocmd.lua
+++ config/nvim/lua/core/autocmd.lua
@@ -1,9 +1,42 @@
 local auto = require('core.utils.autocmd')
 
 -- Highlight search only searching
+local update_search_higlight = function(event)
+    local cmdtype = vim.fn.getcmdtype()
+    if cmdtype ~= '/' and cmdtype ~= '?' then
+        return
+    end
+
+    if event == 'enter' then
+        vim.opt.hlsearch = true
+    elseif event == 'change' then
+        if vim.fn.exists('minimap#vim#UpdateColorSearch') then
+            -- For some way NeoVim set v:hlsearch to 0 at every command line
+            -- change but minimap relies on v:hlsearch being 1 so set hlsearch
+            -- again set v:hlsearch to 1.
+            vim.opt.hlsearch = true
+
+            vim.fn['minimap#vim#UpdateColorSearch'](vim.fn.getcmdline())
+        end
+    elseif event == 'leave' then
+        vim.opt.hlsearch = false
+    end
+end
+
+
 auto.group('VimHighlightOnSearch')
-auto.cmd('CmdlineEnter', '/,?', 'set hlsearch', 'VimHighlightOnSearch')
-auto.cmd('CmdlineLeave', '/,?', 'set nohlsearch', 'VimHighlightOnSearch')
+auto.cmd('CmdlineEnter', '/,?', function(args)
+    update_search_higlight('enter')
+end, 'VimHighlightOnSearch')
+
+auto.cmd('CmdlineChanged', '/,?', function()
+    update_search_higlight('change')
+end, 'VimHighlightOnSearch')
+
+auto.cmd('CmdlineLeave', '/,?', function()
+    update_search_higlight('leave')
+end, 'VimHighlightOnSearch')
+
 
 -- Timeout
 local update_timeoutlen = function()
