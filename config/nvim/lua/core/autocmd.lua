6a7a352e423f85e36b3b2f8d0cf8f47494bec0eb
Jonatha Gabriel <jonathagabrielns@gmail.com>
2026-01-03 20:00:54 -0300

    fix: autocmd for / and ? executing minimap always

--- config/nvim/lua/core/autocmd.lua
+++ config/nvim/lua/core/autocmd.lua
@@ -10,7 +10,7 @@ local update_search_higlight = function(event)
     if event == 'enter' then
         vim.opt.hlsearch = true
     elseif event == 'change' then
-        if vim.fn.exists('minimap#vim#UpdateColorSearch') then
+        if vim.fn.exists('minimap#vim#UpdateColorSearch') ~= 0 then
             -- For some way NeoVim set v:hlsearch to 0 at every command line
             -- change but minimap relies on v:hlsearch being 1 so set hlsearch
             -- again set v:hlsearch to 1.
