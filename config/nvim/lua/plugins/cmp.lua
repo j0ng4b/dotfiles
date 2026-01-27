3b2331e4c337ef29cfd0627dc32a8a5e21eaa4c2
Jonatha Gabriel <60396154+j0ng4b@users.noreply.github.com>
2026-01-27 19:24:44 -0300

    remove(nvim): SQL completion sources from cmp configuration
    Remove SQL-related completion sources from cmp setup.

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -122,14 +122,6 @@ local config = function()
         }),
     })
 
-
-    cmp.setup.filetype({ 'sql', 'mysql', 'plsql' }, {
-        sources = {
-            { name = 'vim-dadbod-completion' },
-        }
-    })
-
-
     cmp.event:on('menu_opened', function()
         vim.b.copilot_suggestion_hidden = true
     end)
