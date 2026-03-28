be6eb7aaf0d72fade7f0520938af5e7f605561e8
Jonatha Gabriel <jonathagabrielns@gmail.com>
2026-03-27 22:18:34 -0300

    feat(nvim): update nvim plugins and add commands to (dis)enable conform

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -1,20 +1,21 @@
 local config = function()
-    local conform = require('conform')
+    local conform = require("conform")
 
     conform.setup({
         formatters_by_ft = {
-            html            = { 'prettier' },
-            htmldjango      = { 'prettier' },
-            jinja           = { 'prettier' },
-            css             = { 'prettier' },
-            javascript      = { 'prettier' },
-            typescript      = { 'prettier' },
-            javascriptreact = { 'prettier' },
-            typescriptreact = { 'prettier' },
-            json            = { 'prettier' },
-            yaml            = { 'prettier' },
-            markdown        = { 'prettier' },
-            python          = { 'isort', 'black' },
+            html = { "prettier" },
+            htmldjango = { "prettier" },
+            jinja = { "prettier" },
+            css = { "prettier" },
+            javascript = { "prettier" },
+            typescript = { "prettier" },
+            javascriptreact = { "prettier" },
+            typescriptreact = { "prettier" },
+            json = { "prettier" },
+            yaml = { "prettier" },
+            markdown = { "prettier" },
+            lua = { "stylua" },
+            python = { "isort", "black" },
         },
 
         format_after_save = {
@@ -22,17 +23,35 @@ local config = function()
         },
     })
 
-    vim.keymap.set({ 'n', 'v' }, '<leader>mp', function()
+    vim.keymap.set({ "n", "v" }, "<leader>mp", function()
         conform.format({
             lsp_fallback = true,
             async = false,
             timeout_ms = 500,
         })
-    end, { desc = 'Format file or range (in visual mode)' })
+    end, { desc = "Format file or range (in visual mode)" })
+
+    vim.api.nvim_create_user_command("ConformDisable", function(args)
+        if args.bang then
+            vim.b.disable_autoformat = true
+        else
+            vim.g.disable_autoformat = true
+        end
+    end, {
+        desc = "Disable autoformat-on-save",
+        bang = true,
+    })
+
+    vim.api.nvim_create_user_command("ConformEnable", function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+    end, {
+        desc = "Re-enable autoformat-on-save",
+    })
 end
 
 return {
-    'stevearc/conform.nvim',
+    "stevearc/conform.nvim",
     config = config,
-    event = { 'BufReadPre', 'BufNewFile' },
+    event = { "BufReadPre", "BufNewFile" },
 }
