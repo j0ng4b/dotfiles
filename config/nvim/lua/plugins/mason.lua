e8b23cd1118e149507492fdb3a5b0d0eb39f759d
Jonatha Gabriel <jonathagabrielns@gmail.com>
2026-04-03 15:08:20 -0300

    feat(nvim): mason need to run first

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -1,5 +1,6 @@
 return {
     "mason-org/mason.nvim",
+    priority = 100,
     dependencies = { "WhoIsSethDaniel/mason-tool-installer.nvim" },
     config = function()
         require("mason").setup()
