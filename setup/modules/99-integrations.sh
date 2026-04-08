920231e18200782c012cf3275a3e91c0b202c735
Jonatha Gabriel <jonathagabrielns@gmail.com>
2026-04-08 00:09:59 -0300

    feat(setup): addded udev rules to control backlight and leds

--- setup/modules/99-integrations.sh
+++ setup/modules/99-integrations.sh
@@ -9,5 +9,17 @@ setup_integrations() {
     _info "Finalizing user group memberships..."
     user_add_groups "$username" docker _seatd bluetooth
 
+    _info "Setting up udev rules for backlight and LEDs..."
+    _run write_file_root /etc/udev/rules.d/99-backlight-leds.rules 0644 root:root <<'EOF'
+ACTION=="add", SUBSYSTEM=="backlight", RUN+="/bin/chgrp video /sys/class/backlight/%k/brightness"
+ACTION=="add", SUBSYSTEM=="backlight", RUN+="/bin/chmod g+w /sys/class/backlight/%k/brightness"
+ACTION=="add", SUBSYSTEM=="leds", RUN+="/bin/chgrp input /sys/class/leds/%k/brightness"
+ACTION=="add", SUBSYSTEM=="leds", RUN+="/bin/chmod g+w /sys/class/leds/%k/brightness"
+EOF
+
+    _info "Reloading udev rules..."
+    _run udevadm control --reload-rules >/dev/null 2>&1 || \
+        _warn "Failed to reload udev rules"
+
     _info "Setup integrations completed"
 }
