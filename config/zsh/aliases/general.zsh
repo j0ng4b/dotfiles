21bae1acb3274f29afd5232abc99dba9eacebefc
Jonatha Gabriel <jonathagabrielns@gmail.com>
2026-04-14 23:37:46 -0300

    feat(zsh): update some alias

--- config/zsh/aliases/general.zsh
+++ config/zsh/aliases/general.zsh
@@ -1,17 +1,29 @@
+## sudo
+alias sudo="sudo "
+
+
 ## ls
-_ls_default_opts="--all --icons --group-directories-first"
-_ls_default_long_opts="--long --smart-group --total-size --git"
+if command -v eza >/dev/null 2>&1; then
+    _ls_default_opts="--all --icons --group-directories-first"
+    _ls_default_long_opts="--long --smart-group --total-size --git"
 
-alias -g ls="eza $_ls_default_opts --classify=always"
-alias -g ll="eza $_ls_default_opts $_ls_default_long_opts"
+    alias ls="eza $_ls_default_opts --classify=always"
+    alias ll="eza $_ls_default_opts $_ls_default_long_opts"
 
-alias -g lt="eza $_ls_default_opts --tree --level=2"
-alias -g llt="eza $_ls_default_opts $_ls_default_long_opts --tree --level=2"
+    alias lt="eza $_ls_default_opts --tree --level=2"
+    alias llt="eza $_ls_default_opts $_ls_default_long_opts --tree --level=2"
 
-alias -g lr="eza $_ls_default_opts --recurse --level=2"
-alias -g llr="eza $_ls_default_opts $_ls_default_long_opts --recurse --level=2"
+    alias lr="eza $_ls_default_opts --recurse --level=2"
+    alias llr="eza $_ls_default_opts $_ls_default_long_opts --recurse --level=2"
 
-alias -g lg="eza $_ls_default_opts $_ls_default_long_opts --git-repos"
+    alias lg="eza $_ls_default_opts $_ls_default_long_opts --git-repos"
+else
+    alias ls="ls -FA"
+    alias ll="ls -FAl"
+
+    alias lr="ls -AFR"
+    alias llr="ls -FARl"
+fi
 
 
 ## cat
--- config/zsh/aliases/xbps.zsh
+++ config/zsh/aliases/xbps.zsh
@@ -1,53 +1,53 @@
 # xbps-install
 ## synchronize remote repository index files.
-alias -g xi="xbps-install --sync"
+alias xi="xbps-install --sync"
 
 ## same as above but also update all system installed packages or a
 ## specific package.
-alias -g xiu="xbps-install --sync --update"
+alias xiu="xbps-install --sync --update"
 
 
 # xbps-remove
 ## recursively remove packages that were installed by a package and
 ## aren't required by other installed packages.
-alias -g xr="xbps-remove --recursive"
+alias xr="xbps-remove --recursive"
 
 ## removes installed package orphans that were installed automatically
 ## and outdated binary packages from cache.
-alias -g xrc="xbps-remove --clean-cache  --remove-orphans"
+alias xrc="xbps-remove --clean-cache  --remove-orphans"
 
 ## same as above but also remove packages that are not installed from
 ## the cache.
-alias -g xrC="xbps-remove --clean-cache --clean-cache --remove-orphans"
+alias xrC="xbps-remove --clean-cache --clean-cache --remove-orphans"
 
 
 # xbps-query
 ## search for packages by matching
-alias -g xq="xbps-query -R --regex --search"
+alias xq="xbps-query -R --regex --search"
 
 ## lists registered packages in the package database
-alias -g xql="xbps-query -R --list-pkgs"
+alias xql="xbps-query -R --list-pkgs"
 
 ## lists registered packages that were installed manually
-alias -g xqm="xbps-query -R --list-manual-pkgs"
+alias xqm="xbps-query -R --list-manual-pkgs"
 
 ## lists registered packages that were installed manually
-alias -g xqo="xbps-query -R --list-orphans"
+alias xqo="xbps-query -R --list-orphans"
 
 ## shows information of an installed package
-alias -g xqs="xbps-query --show"
+alias xqs="xbps-query --show"
 
 ## shows information of a repository package
-alias -g xqS="xbps-query -R --show"
+alias xqS="xbps-query -R --show"
 
 ## show the package files
-alias -g xqf="xbps-query -R --files"
+alias xqf="xbps-query -R --files"
 
 ## prints the some file stored in binary package to stdout
-alias -g xqF="xbps-query -R --cat"
+alias xqF="xbps-query -R --cat"
 
 ## show the required dependencies for a package
-alias -g xqd="xbps-query -R --fulldeptree --deps"
+alias xqd="xbps-query -R --fulldeptree --deps"
 
 ## show the reverse dependencies for a package
-alias -g xqx="xbps-query -R --revdeps"
+alias xqx="xbps-query -R --revdeps"
