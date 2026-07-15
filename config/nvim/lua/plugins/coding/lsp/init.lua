a30adc7638cbde9c3ba549e11dcc1ffcb873a3b3
Jonatha Gabriel <jonathagabrielns@gmail.com>
2026-07-15 20:04:34 -0300

    feat(nvim): removed omnisharp config to use roslyn

--- config/nvim/lua/plugins/coding/lsp/init.lua
+++ config/nvim/lua/plugins/coding/lsp/init.lua
@@ -166,10 +166,27 @@ local server_configs = {
         return { filetypes = filetypes }
     end,
 
-    omnisharp = function(_)
-        local omnisharp_root = vim.fn.stdpath("data") .. "/mason/packages/omnisharp/OmniSharp"
+    roslyn = function(_)
         return {
-            cmd = { omnisharp_root },
+            settings = {
+                ["csharp|completion"] = {
+                    dotnet_show_completion_items_from_unimported_namespaces = true,
+                    dotnet_show_name_completion_suggestions = true,
+                },
+
+                ["csharp|inlay_hints"] = {
+                    csharp_enable_inlay_hints_for_implicit_object_creation = true,
+                    csharp_enable_inlay_hints_for_implicit_variable_types = true,
+                    csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+
+                    dotnet_enable_inlay_hints_for_parameters = true,
+                    dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+                },
+
+                ["csharp|symbol_search"] = {
+                    dotnet_search_reference_assemblies = true,
+                },
+            },
         }
     end,
 
